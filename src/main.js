d247c704ffff240ae47683212b3134687aa43b99
gcolombel.pro <gcolombel.pro@gmail.com>
2025-01-17 14:47:44 +0100

    Projet initial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/css/style.css'
+import './assets/css/reset.css'
+import './assets/css/fonts.css'
 
 createApp(App).mount('#app')
